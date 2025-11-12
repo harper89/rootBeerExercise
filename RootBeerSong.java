9bf3b2112692ea8b7794b5461f377e611720bc5e
Harper <harperlpeters89@gmail.com>
2025-11-12 09:00:23 -0800

    Create RootBeerSong.java

---
+++ RootBeerSong.java
@@ -0,0 +1,18 @@
+public class RootBeerSong {
+   public static void main(String[] args) {
+      singRootBeerSong(5);
+   
+   }
+   public static void singRootBeerSong(int numOfBottles){
+      while (numOfBottles>0) {
+         System.out.println(numOfBottles + " bottles of root beer on the wall");
+         System.out.println(numOfBottles + " bottles of root beer");
+         System.out.println("Take one down, pass it around");
+         System.out.println((numOfBottles-1) + " bottles of root beer on the wall\n");
+         numOfBottles--;
+
+
+      }
+   
+   }
+}
